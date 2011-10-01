39a0b4bfa8b66c74f2eb359f59b09019b77030d6
Ken Erickson <bookworm.productions@gmail.com>
2011-10-01 14:46:47 -0700

    KControllerBehavior

---
+++ code/libraries/koowa/controller/behavior/behavior.php
@@ -0,0 +1,22 @@
+<?php 
+
+class KControllerBehavior
+{
+  public static function factory($behavior, $config = array())
+	{		
+    if(!($behavior instanceof KControllerBehaviorInterface))
+    {   
+      if(is_string($behavior) && strpos($behavior, '.') === false )
+        $behavior = 'com.default.controller.behavior.'.trim($behavior);  
+
+      $behavior = KFactory::tmp($behavior, $config);
+
+      if(!($behavior instanceof KControllerBehaviorInterface)) {
+        $identifier = $behavior->getIdentifier();
+        throw new KControllerBehaviorException("Controller behavior $identifier does not implement KControllerBehaviorInterface");
+      }  
+    }
+
+    return $behavior;  
+	}
+}
