07d423b94c921792877fd878b1df3149a29b83aa
Ken Erickson <bookworm.productions@gmail.com>
2011-10-01 08:18:21 -0700

    KHttpRequest

---
+++ code/libraries/koowa/http/request.php
@@ -0,0 +1,11 @@
+<?php
+
+class KHttpRequest
+{
+  const GET     = 'GET';  
+  const POST    = 'POST';  
+  const PUT     = 'PUT';  
+  const DELETE  = 'DELETE';  
+  const HEAD    = 'HEAD';  
+  const OPTIONS = 'OPTIONS';  
+}
