54909aa04911a728d1544ba8e4342631b4a626bd
Ken Erickson <bookworm.productions@gmail.com>
2011-10-01 11:45:51 -0700

    KFactoryAdapterKoowa

---
+++ code/libraries/koowa/factory/adapter/koowa.php
@@ -0,0 +1,25 @@
+<?php
+
+class KFactoryAdapterKoowa extends KFactoryAdapterAbstract
+{
+  public function instantiate($identifier, KConfig $config)
+	{
+		$classname = false;
+
+		if($identifier->type == 'lib' && $identifier->package == 'koowa')
+		{
+			$classname = 'K'.KInflector::implode($identifier->path).ucfirst($identifier->name);
+			$filepath  = KLoader::path($identifier);
+			
+			if (!class_exists($classname))
+			{
+				$classname = 'K'.KInflector::implode($identifier->path).'Default';
+				
+				if (!class_exists($classname)) 
+  				throw new KFactoryAdapterException("Class [$classname] not found in file [".basename($filepath)."]" );
+			}
+		}
+
+		return $classname;
+	}
+}
