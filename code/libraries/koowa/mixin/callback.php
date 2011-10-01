b7de5e464dc51baca6470563ef0929eca08e3cf6
Ken Erickson <bookworm.productions@gmail.com>
2011-10-01 16:28:35 -0700

    callbacks

--- code/libraries/koowa/mixin/callback.php
+++ code/libraries/koowa/mixin/callback.php
@@ -7,36 +7,36 @@ class KMixinCallback extends KMixinAbstract implements KCommandInterface
   protected $_priority;    
   
   public function __construct(KConfig $config)
-	{
-  	parent::__construct($config);
-      
-  	if(is_null($config->command_chain))
-  		throw new KMixinException('command_chain [KCommandChain] option is required');
-	
-  	$this->_priority = $config->command_priority;
-
-  	$config->command_chain->enqueue($this);     
-	}   
-	
-	protected function _initialize(KConfig $config)
   {
-  	$config->append(array(
-  		'command_chain'		=> null,
-  		'command_priority'	=> KCommand::PRIORITY_HIGH
-  	));
-  	
-  	parent::_initialize($config);
+    parent::__construct($config);
+      
+    if(is_null($config->command_chain))
+      throw new KMixinException('command_chain [KCommandChain] option is required');
+  
+    $this->_priority = $config->command_priority;
+
+    $config->command_chain->enqueue($this);     
+  }   
+  
+  protected function _initialize(KConfig $config)
+  {
+    $config->append(array(
+      'command_chain'   => null,
+      'command_priority'  => KCommand::PRIORITY_HIGH
+    ));
+    
+    parent::_initialize($config);
   } 
   
   public function execute( $name, KCommandContext $context) 
-	{
+  {
     $result = true;   
 
     if(isset($this->_callbacks[$name])) 
     {
       $callbacks = $this->_callbacks[$name];
       $params    = $this->_params[$name]; 
-		
+    
       foreach($callbacks as $key => $callback) 
       {
         $param = $params[$key]; 
@@ -45,56 +45,53 @@ class KMixinCallback extends KMixinAbstract implements KCommandInterface
         else$result = call_user_func($callback,  $context->append($param));
 
         if ($result === false) break;   
-    	}
+      }
     }
 
     return $result === false ? false : true;   
-	}    
-	
-	public function getCallbacks($command)
-	{
-		$result = array();
-		$command = strtolower($command);
-		
-		if (isset($this->_callbacks[$command])) $result = $this->_callbacks[$command];
-		
+  }    
+  
+  public function getCallbacks($command)
+  {
+    $result = array();
+    $command = strtolower($command);
+    
+    if (isset($this->_callbacks[$command])) $result = $this->_callbacks[$command];
+    
     return $result;
-	}    
-	
-	public function registerCallback($commands, $callback, $params = array())
-	{
-		$commands = (array) $commands;
-		$params  = (array) KConfig::toData($params); 
-		
-		foreach($commands as $command)
-		{
-			$command = strtolower($command);
-		
-			if (!isset($this->_callbacks[$command]) ) 
-			{
-       	 		$this->_callbacks[$command] = array();
-       	 		$this->_params[$command]   = array(); 	
-			}
-			
-			//Don't re-register commands.
-			$index = array_search($callback, $this->_callbacks[$command], true);
+  }    
+  
+  public function registerCallback($commands, $callback, $params = array())
+  {
+    $commands = (array) $commands;
+    $params  = (array) KConfig::toData($params); 
+    
+    foreach($commands as $command)
+    {
+      $command = strtolower($command);
+    
+      if(!isset($this->_callbacks[$command]) ) 
+      {
+        $this->_callbacks[$command] = array();
+        $this->_params[$command]   = array();   
+      }        
+      
+      $index = array_search($callback, $this->_callbacks[$command], true);
 
-			if ( $index === false ) 
-			{ 
-		        $this->_callbacks[$command][] = $callback;
-    		    $this->_params[$command][]    = $params; 
-			}
-			else  
-			{
-			   $this->_params[$command][$index] = array_merge($this->_params[$command][$index], $params); 
-			}
-		}
-		
-		return $this->_mixer;
-	}
+      if($index === false) { 
+        $this->_callbacks[$command][] = $callback;
+        $this->_params[$command][]    = $params; 
+      }     
+      else {
+         $this->_params[$command][$index] = array_merge($this->_params[$command][$index], $params); 
+      }
+    }
+    
+    return $this->_mixer;
+  }
 
-	public function unregisterCallback($commands, $callback)
-	{
+  public function unregisterCallback($commands, $callback)
+  {
     $commands = (array) $commands;
 
     foreach($commands as $command)
@@ -110,8 +107,8 @@ class KMixinCallback extends KMixinAbstract implements KCommandInterface
     }
 
     return $this->_mixer; 
-	} 
-		
+  } 
+    
   public function getMixableMethods(KObject $mixer = null) 
   {
     return array_diff(parent::getMixableMethods(), array('execute', 'getPriority'));  
