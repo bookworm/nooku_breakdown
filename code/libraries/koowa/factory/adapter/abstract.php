6213f2a9bc4ad7c3bbbd5694db79119fb41ca59c
Ken Erickson <bookworm.productions@gmail.com>
2011-10-01 11:37:34 -0700

    KFactoryAdapterAbstract

---
+++ code/libraries/koowa/factory/adapter/abstract.php
@@ -0,0 +1,33 @@
+<?php
+
+abstract class KFactoryAdapterAbstract extends KObject implements KFactoryAdapterInterface
+{
+	protected $_priority;
+
+	public function __construct( KConfig $config = null) 
+	{ 
+    if(!isset($config)) $config = new KConfig();
+    parent::__construct($config);
+    $this->_priority = $config->priority;  
+	}
+
+  protected function _initialize(KConfig $config)
+  {
+    $config->append(array(
+      'priority'   => KCommand::PRIORITY_NORMAL,
+    ));
+
+    parent::_initialize($config);    
+  }
+
+  final public function execute($identifier, KCommandContext $context)
+  {
+    $result = $this->instantiate($identifier, $context->config);
+    return $result;
+  }
+
+  public function getPriority()
+  {
+    return $this->_priority;
+	}
+}
