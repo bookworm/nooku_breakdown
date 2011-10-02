5e2a5f96b4a59e2629d80e2739a6d52ad11545ec
Ken Erickson <bookworm.productions@gmail.com>
2011-10-02 12:58:57 -0700

    KViewHtml

---
+++ code/libraries/koowa/view/html.php
@@ -0,0 +1,37 @@
+<?php 
+
+class KViewHtml extends KViewTemplate
+{
+  protected function _initialize(KConfig $config)
+  {
+    $config->append(array(
+      'mimetype'	  		 => 'text/html',
+      'template_filters' => array('form'),
+    ));
+
+    parent::_initialize($config); 
+  }   
+	
+	public function display()
+	{
+    if(empty($this->output))
+    {
+      $model = $this->getModel();
+
+      $this->assign('state', $model->getState());
+
+      if($this->_auto_assign)
+      {
+        $name  = $this->getName();
+
+        if(KInflector::isPlural($name)) {
+          $this->assign($name, 	$model->getList())
+            ->assign('total',	$model->getTotal());   
+        }
+        else $this->assign($name, $model->getItem());    
+      }  
+    }
+
+    return parent::display();  
+	}
+}
