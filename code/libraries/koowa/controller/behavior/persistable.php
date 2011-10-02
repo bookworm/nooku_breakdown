e0b1fa806e54199b14703cd93f3bed218f13632e
Ken Erickson <bookworm.productions@gmail.com>
2011-10-01 17:23:01 -0700

    KControllerBehaviorPersistable

---
+++ code/libraries/koowa/controller/behavior/persistable.php
@@ -0,0 +1,23 @@
+<?php 
+
+class KControllerBehaviorPersistable extends KControllerBehaviorAbstract
+{
+  protected function _beforeBrowse(KCommandContext $context)
+	{
+		$identifier = $this->getModel()->getIdentifier().'.'.$context->action;
+		$state      = KRequest::get('session.'.$identifier, 'raw', array());
+			
+		$this->getRequest()->append($state);
+		$this->getModel()->set($this->getRequest());
+	}
+	
+	protected function _afterBrowse(KCommandContext $context)
+	{
+		$model = $this->getModel();
+		$state = $model->get();
+
+		$identifier = $model->getIdentifier().'.'.$context->action;
+		
+		KRequest::set('session.'.$identifier, $state);
+	}
+}
