8eda84e1ec17fbada7167eb3836911878ce015a5
Ken Erickson <bookworm.productions@gmail.com>
2011-10-02 11:13:28 -0700

    KDatabaseBehavior

--- code/libraries/koowa/database/behavior/behavior.php
+++ code/libraries/koowa/database/behavior/behavior.php
@@ -1,5 +1,22 @@
 <?php
 
 class KDatabaseBehavior
-{
+{ 
+  public static function factory($behavior, $config = array())
+	{		
+    if(!($behavior instanceof KDatabaseBehaviorInterface))
+    {   
+      if(is_string($behavior) && strpos($behavior, '.') === false ) 
+        $behavior = 'com.default.database.behavior.'.trim($behavior);  
+
+      $behavior = KFactory::tmp($behavior, $config);
+
+      if(!($behavior instanceof KDatabaseBehaviorInterface)) {
+        $identifier = $behavior->getIdentifier();
+        throw new KDatabaseBehaviorException("Database behavior $identifier does not implement KDatabaseBehaviorInterface");
+      }  
+    }
+
+    return $behavior;    
+	}
 }
