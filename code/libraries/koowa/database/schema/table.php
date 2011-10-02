00e7ea6fc8f56ab375a805489c89b94eee85f403
Ken Erickson <bookworm.productions@gmail.com>
2011-10-02 10:02:36 -0700

    KDatabaseSchemaTable

---
+++ code/libraries/koowa/database/schema/table.php
@@ -0,0 +1,15 @@
+<?php
+
+class KDatabaseSchemaTable extends KObject
+{
+	public $name; 
+	public $engine;
+	public $type;
+	public $length;
+	public $autoinc;
+	public $collation;
+	public $description;
+	public $columns = array();
+	public $behaviors = array();
+	public $indexes = array();
+}
