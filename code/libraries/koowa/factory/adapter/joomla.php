f8d367861aa3003789aff12a40f3ee751e3cca6f
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 11:54:18 -0700

    KFactoryAdapterJoomla tabs to spaces

--- code/libraries/koowa/factory/adapter/joomla.php
+++ code/libraries/koowa/factory/adapter/joomla.php
@@ -5,23 +5,23 @@ class KFactoryAdapterJoomla extends KFactoryAdapterAbstract
   protected $_alias_map = array(
     'Database'      => 'DBO',
     'Authorization' => 'ACL',
-    'Xml'    	    	=> 'XMLParser'   
-	);  
-	
-	public function instantiate($identifier, KConfig $config)
-	{
-		$instance = false;
+    'Xml'           => 'XMLParser'   
+  );  
+  
+  public function instantiate($identifier, KConfig $config)
+  {
+    $instance = false;
 
     if($identifier->type == 'lib' && $identifier->package == 'joomla')
     {
       $name = ucfirst($identifier->name);
 
       if(array_key_exists($name, $this->_alias_map)) 
-      	$name = $this->_alias_map[$name];
+        $name = $this->_alias_map[$name];
 
       $instance = call_user_func_array(array('JFactory', 'get'.$name), $config->toArray());
     }
 
     return $instance; 
-	}
+  }
 }
