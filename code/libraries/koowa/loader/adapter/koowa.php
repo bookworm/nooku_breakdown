6d8445edf846b87836c2a9dd6945e8a37d3cf4c5
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 12:12:50 -0700

    KLoaderAdapterKoowa tabs to spaces

--- code/libraries/koowa/loader/adapter/koowa.php
+++ code/libraries/koowa/loader/adapter/koowa.php
@@ -2,42 +2,42 @@
 
 class KLoaderAdapterKoowa extends KLoaderAdapterAbstract
 {
-	protected $_prefix = 'K';
+  protected $_prefix = 'K';
 
-	protected function _pathFromClassname($classname)
-	{
-		$path     = false;
-		
-		$word  = preg_replace('/(?<=\\w)([A-Z])/', '_\\1',  $classname);
-		$parts = explode('_', $word);
-		
-		if(array_shift($parts) == $this->_prefix)
-		{	
-			$path = strtolower(implode('/', $parts));
-				
-			if(count($parts) == 1) $path = $path.'/'.$path;
-			
-			if(!is_file($this->_basepath.'/'.$path.'.php')) $path = $path.'/'.strtolower(array_pop($parts));
+  protected function _pathFromClassname($classname)
+  {
+    $path     = false;
+    
+    $word  = preg_replace('/(?<=\\w)([A-Z])/', '_\\1',  $classname);
+    $parts = explode('_', $word);
+    
+    if(array_shift($parts) == $this->_prefix)
+    { 
+      $path = strtolower(implode('/', $parts));
+        
+      if(count($parts) == 1) $path = $path.'/'.$path;
+      
+      if(!is_file($this->_basepath.'/'.$path.'.php')) $path = $path.'/'.strtolower(array_pop($parts));
 
-			$path = $this->_basepath.'/'.$path.'.php';
-		}
-		
-		return $path;
-	}	
+      $path = $this->_basepath.'/'.$path.'.php';
+    }
+    
+    return $path;
+  } 
 
-	protected function _pathFromIdentifier($identifier)
-	{
-		$path = false;
-		
-		if($identifier->type == 'lib' && $identifier->package == 'koowa')
-		{
-			if(count($identifier->path)) $path .= implode('/',$identifier->path);
+  protected function _pathFromIdentifier($identifier)
+  {
+    $path = false;
+    
+    if($identifier->type == 'lib' && $identifier->package == 'koowa')
+    {
+      if(count($identifier->path)) $path .= implode('/',$identifier->path);
 
-			if(!empty($identifier->name)) $path .= '/'.$identifier->name;
-				
-			$path = $this->_basepath.'/'.$path.'.php';
-		}
-		
-		return $path;
-	}
+      if(!empty($identifier->name)) $path .= '/'.$identifier->name;
+        
+      $path = $this->_basepath.'/'.$path.'.php';
+    }
+    
+    return $path;
+  }
 }
