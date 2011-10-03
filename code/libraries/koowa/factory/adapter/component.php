0fefb42fcc3adfc312aab32d8774449aa5dc2936
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 11:53:10 -0700

    KFactoryAdapterComponent tabs to spaces

--- code/libraries/koowa/factory/adapter/component.php
+++ code/libraries/koowa/factory/adapter/component.php
@@ -3,19 +3,19 @@
 class KFactoryAdapterComponent extends KFactoryAdapterAbstract
 {
   public function instantiate($identifier, KConfig $config)
-	{
-		$classname = false;
-		
-		if($identifier->type == 'com') 
-		{			
-			$path      = KInflector::camelize(implode('_', $identifier->path));
-    	$classname = 'Com'.ucfirst($identifier->package).$path.ucfirst($identifier->name);
-        		        	
-			if(!class_exists($classname, false))
-			{
+  {
+    $classname = false;
+    
+    if($identifier->type == 'com') 
+    {     
+      $path      = KInflector::camelize(implode('_', $identifier->path));
+      $classname = 'Com'.ucfirst($identifier->package).$path.ucfirst($identifier->name);
+                      
+      if(!class_exists($classname, false))
+      {
         if($path = KLoader::load($identifier)) {
-        	if (!class_exists($classname, false))
-        		throw new KFactoryAdapterException("Class [$classname] not found in file [".$path."]" );
+          if (!class_exists($classname, false))
+            throw new KFactoryAdapterException("Class [$classname] not found in file [".$path."]" );
         }
         else 
         {
@@ -25,7 +25,7 @@ class KFactoryAdapterComponent extends KFactoryAdapterAbstract
           $path = ($classtype != 'view') ? KInflector::camelize(implode('_', $classpath)) : '';
 
           if(class_exists('Com'.ucfirst($identifier->package).ucfirst($classtype).$path.ucfirst($identifier->name)))
-          	$classname = 'Com'.ucfirst($identifier->package).ucfirst($classtype).$path.ucfirst($identifier->name);
+            $classname = 'Com'.ucfirst($identifier->package).ucfirst($classtype).$path.ucfirst($identifier->name);
           elseif(class_exists('Com'.ucfirst($identifier->package).ucfirst($classtype).$path.'Default')) 
             $classname = 'Com'.ucfirst($identifier->package).ucfirst($classtype).$path.'Default'; 
           elseif(class_exists('ComDefault'.ucfirst($classtype).$path.ucfirst($identifier->name)))
@@ -38,9 +38,9 @@ class KFactoryAdapterComponent extends KFactoryAdapterAbstract
             $classname = 'K'.ucfirst($classtype).$path.'Default';
           else $classname = false; 
         } 
-			}
-		}
+      }
+    }
 
-		return $classname;
-	}
+    return $classname;
+  }
 }
