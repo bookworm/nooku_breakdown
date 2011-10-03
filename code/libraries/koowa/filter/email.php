2176afdcb9450fcc750842f60ea3eff69e506ad3
Ken Erickson <bookworm.productions@gmail.com>
2011-10-02 19:19:31 -0700

    KFilterEmail

---
+++ code/libraries/koowa/filter/email.php
@@ -0,0 +1,16 @@
+<?php
+
+class KFilterEmail extends KFilterAbstract
+{
+  protected function _validate($value)
+	{
+		$value = trim($value);
+		return (false !== filter_var($value, FILTER_VALIDATE_EMAIL));
+	}
+	
+	protected function _sanitize($value)
+	{
+		$value = trim($value);
+		return filter_var($value, FILTER_SANITIZE_EMAIL);
+	}
+}
