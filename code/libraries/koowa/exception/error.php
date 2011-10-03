0cca71ea06ba26769c562c4a7a3fe07fb555ec46
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 11:51:06 -0700

    KExceptionError

--- code/libraries/koowa/exception/error.php
+++ code/libraries/koowa/exception/error.php
@@ -4,7 +4,7 @@ class KExceptionError extends ErrorException implements KExceptionInterface
 {
   public function __construct($message, $code, $severity, $filename, $lineno)
   {
-    if (!$message) 
+    if(!$message) 
       throw new $this('Unknown '. get_class($this));
 
     parent::__construct($message, $code, $severity, $filename, $lineno);    
@@ -13,8 +13,8 @@ class KExceptionError extends ErrorException implements KExceptionInterface
   public function __toString()
   {        
     return "exception '".get_class($this) ."' with message '".$this->getMessage()
-          ."' in ".$this->getFile().":".$this->getLine()
-          ."\nStack trace:\n"
-          . "  " . str_replace("\n", "\n  ", $this->getTraceAsString());  
+      ."' in ".$this->getFile().":".$this->getLine()
+      ."\nStack trace:\n"
+      . "  " . str_replace("\n", "\n  ", $this->getTraceAsString());  
   } 
 }
