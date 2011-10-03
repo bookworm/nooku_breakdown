9d3adecd373e18ab9cb689d4457b888e8b6034b0
Ken Erickson <bookworm.productions@gmail.com>
2011-10-02 19:22:24 -0700

    KFilterFilename

---
+++ code/libraries/koowa/filter/filename.php
@@ -0,0 +1,14 @@
+<?php
+
+class KFilterFilename extends KFilterAbstract
+{ 
+  protected function _validate($value)
+	{
+   	return ((string) $value === $this->sanitize($value));
+	}                     
+	
+	protected function _sanitize($value)
+	{
+    return basename($value);
+	}
+}
