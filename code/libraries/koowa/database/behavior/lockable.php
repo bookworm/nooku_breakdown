78012dcf113efbc398a18e94f23ef76f01e54d4c
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 11:17:49 -0700

    KDatabaseBehaviorLockable tabs to spaces

--- code/libraries/koowa/database/behavior/lockable.php
+++ code/libraries/koowa/database/behavior/lockable.php
@@ -3,12 +3,12 @@
 class KDatabaseBehaviorLockable extends KDatabaseBehaviorAbstract
 {
   protected $_lifetime;
-		
-	protected function _initialize(KConfig $config)
+    
+  protected function _initialize(KConfig $config)
   {
     $config->append(array(
       'priority'   => KCommand::PRIORITY_HIGH,
-      'lifetime'	 => '900'
+      'lifetime'   => '900'
     ));
 
     $this->_lifetime = $config->lifetime;
@@ -17,28 +17,28 @@ class KDatabaseBehaviorLockable extends KDatabaseBehaviorAbstract
   }             
   
   public function getMixableMethods(KObject $mixer = null)
-	{
-		$methods = array();
+  {
+    $methods = array();
 
-		if(isset($mixer->locked_by) && isset($mixer->locked_on))
-  		$methods = parent::getMixableMethods($mixer);
+    if(isset($mixer->locked_by) && isset($mixer->locked_on))
+      $methods = parent::getMixableMethods($mixer);
 
-		return $methods;
-	}    
-	
-	public function lock()
-	{
-		if(!$this->isNew() && !$this->locked()) {
+    return $methods;
+  }    
+  
+  public function lock()
+  {
+    if(!$this->isNew() && !$this->locked()) {
       $this->locked_by = (int) KFactory::get('lib.joomla.user')->get('id');
       $this->locked_on = gmdate('Y-m-d H:i:s');
       $this->save();      
-		}
+    }
 
-		return true;
-	}  
-	
-	public function unlock()
- 	{
+    return true;
+  }  
+  
+  public function unlock()
+  {
     $userid = KFactory::get('lib.joomla.user')->get('id');
 
     if(!$this->isNew() && $this->locked_by != 0 && $this->locked_by == $userid)
@@ -49,12 +49,12 @@ class KDatabaseBehaviorLockable extends KDatabaseBehaviorAbstract
     }
 
     return true;
- 	}  
- 	
- 	public function locked()
-	{
-		$result = false;     
-		
+  }  
+  
+  public function locked()
+  {
+    $result = false;     
+    
     if(!$this->isNew())
     {
       if(isset($this->locked_on) && isset($this->locked_by)) 
@@ -71,30 +71,30 @@ class KDatabaseBehaviorLockable extends KDatabaseBehaviorAbstract
     }
 
     return $result;
-	}   
-	
-	public function lockMessage()
-	{
+  }   
+  
+  public function lockMessage()
+  {
     $message = '';
 
     if($this->locked())
     {
-      $user = KFactory::tmp('lib.joomla.user', array($this->locked_by));	    
+      $user = KFactory::tmp('lib.joomla.user', array($this->locked_by));      
       $date = KTemplateHelper::factory('date')->humanize(array('date' => $this->locked_on));
 
       $message = JText::sprintf('Locked by %s %s', $user->get('name'), $date);
     }
 
     return $message;       
-	} 
-	
-	protected function _beforeTableUpdate(KCommandContext $context)
-	{
+  } 
+  
+  protected function _beforeTableUpdate(KCommandContext $context)
+  {
     return (bool) !$this->locked();
-	}          
-	
-	protected function _beforeTableDelete(KCommandContext $context)
-	{
-		return (bool) !$this->locked();
-	}
+  }          
+  
+  protected function _beforeTableDelete(KCommandContext $context)
+  {
+    return (bool) !$this->locked();
+  }
 }
