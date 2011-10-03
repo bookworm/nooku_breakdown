08aca647b40f8a1b511edf41a9803b71a9e5acf5
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 12:02:07 -0700

    KFilterAlpha tabs to spaces

--- code/libraries/koowa/filter/alpha.php
+++ code/libraries/koowa/filter/alpha.php
@@ -3,15 +3,15 @@
 class KFilterAlpha extends KFilterAbstract
 {  
   protected function _validate($value)
-	{
-		$value = trim($value);
-		
-		return ctype_alpha($value);
-	}           
-	
-	protected function _sanitize($value)
-	{
-		$pattern 	= '/[^[a-zA-Z]*/';
-  	return preg_replace($pattern, '', $value);
-	}
+  {
+    $value = trim($value);
+    
+    return ctype_alpha($value);
+  }           
+  
+  protected function _sanitize($value)
+  {
+    $pattern  = '/[^[a-zA-Z]*/';
+    return preg_replace($pattern, '', $value);
+  }
 }
