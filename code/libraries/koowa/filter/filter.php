d9f9d4d5784c790043a5620e8f7618815e1c9ed0
Ken Erickson <bookworm.productions@gmail.com>
2011-10-02 19:42:50 -0700

    KFilter

---
+++ code/libraries/koowa/filter/filter.php
@@ -0,0 +1,39 @@
+<?php
+
+class KFilter
+{
+  public static function factory($identifier, $config = array())
+	{		
+  	$filters = (array) $identifier;
+
+  	$filter = array_shift($filters);
+  	$filter = self::_createFilter($filter, $config);
+	
+  	foreach($filters as $name) {
+  		$filter->addFilter(self::_createFilter($name, $config));
+  	}
+	
+  	return $filter;   
+	}
+	
+	protected static function _createFilter($filter, $config)
+	{
+    try 
+    {
+    	if(is_string($filter) && strpos($filter, '.') === false )
+    		$filter = 'com.default.filter.'.trim($filter);
+
+    	$filter = KFactory::tmp($filter, $config);
+    } 
+    catch(KFactoryAdapterException $e) {
+    	throw new KFilterException('Invalid filter: '.$filter);
+    }
+
+    if(!($filter instanceof KFilterInterface)) {
+    	$identifier = $filter->getIdentifier();
+    	throw new KFilterException("Filter $identifier does not implement KFilterInterface");
+    }
+
+    return $filter;
+	}
+}
