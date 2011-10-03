df1f1d97579d8cae599f16c3051e62c4619ab68b
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 11:43:55 -0700

    KDispatcherAbstract tabs to spaces

--- code/libraries/koowa/dispatcher/abstract.php
+++ code/libraries/koowa/dispatcher/abstract.php
@@ -5,60 +5,60 @@ abstract class KDispatcherAbstract extends KControllerAbstract
   protected $_controller;  
   
   public function __construct(KConfig $config)
-	{
-		parent::__construct($config);
-		
-		$this->_controller = $config->controller;
-		
-		if(KRequest::method() != 'GET') 
-  		$this->registerCallback('after.dispatch' , array($this, 'forward'));
+  {
+    parent::__construct($config);
+    
+    $this->_controller = $config->controller;
+    
+    if(KRequest::method() != 'GET') 
+      $this->registerCallback('after.dispatch' , array($this, 'forward'));
 
     $this->registerCallback('after.dispatch', array($this, 'render'));
-	}       
-	
-	protected function _initialize(KConfig $config)
+  }       
+  
+  protected function _initialize(KConfig $config)
   {
     $config->append(array(
       'controller' => $this->_identifier->package,
-      'request'	   => KRequest::get('get', 'string'),
+      'request'    => KRequest::get('get', 'string'),
     ))->append(array(
-      'request' 	 => array('format' => KRequest::format() ? KRequest::format() : 'html')
+      'request'    => array('format' => KRequest::format() ? KRequest::format() : 'html')
     ));  
 
     parent::_initialize($config);
   } 
   
   public function getController()
-	{
-		if(!($this->_controller instanceof KControllerAbstract))
-		{  
+  {
+    if(!($this->_controller instanceof KControllerAbstract))
+    {  
       if(!($this->_controller instanceof KIdentifier)) 
         $this->setController($this->_controller);
 
       $config = array(
-        'request' 	   => $this->_request,
-        'dispatched'   => true	      
+        'request'      => $this->_request,
+        'dispatched'   => true        
       );
 
       $this->_controller = KFactory::tmp($this->_controller, $config);   
-		}
-	
-		return $this->_controller;
-	}  
-	
-	public function setController($controller)
-	{
-		if(!($controller instanceof KControllerAbstract))
-		{
+    }
+  
+    return $this->_controller;
+  }  
+  
+  public function setController($controller)
+  {
+    if(!($controller instanceof KControllerAbstract))
+    {
       if(is_string($controller) && strpos($controller, '.') === false ) 
       {
         if(KInflector::isPlural($controller)) 
           $controller = KInflector::singularize($controller);
 
 
-        $identifier			= clone $this->_identifier;
-        $identifier->path	= array('controller');
-        $identifier->name	= $controller;   
+        $identifier     = clone $this->_identifier;
+        $identifier->path = array('controller');
+        $identifier->name = $controller;   
       }
       else $identifier = KFactory::identify($controller);
 
@@ -66,15 +66,15 @@ abstract class KDispatcherAbstract extends KControllerAbstract
         throw new KDispatcherException('Identifier: '.$identifier.' is not a controller identifier');
 
       $controller = $identifier;
-		}
-		
-		$this->_controller = $controller;
-	
-		return $this;
-	}
-	
-	protected function _actionDispatch(KCommandContext $context)
-	{        	 
+    }
+    
+    $this->_controller = $controller;
+  
+    return $this;
+  }
+  
+  protected function _actionDispatch(KCommandContext $context)
+  {          
     $action = KRequest::get('post.action', 'cmd', strtolower(KRequest::method()));
 
     if(KRequest::method() != KHttpRequest::GET)
@@ -83,24 +83,24 @@ abstract class KDispatcherAbstract extends KControllerAbstract
     $result = $this->getController()->execute($action, $context);
    
     return $result;
-	}     
-	
-	public function _actionForward(KCommandContext $context)
-	{
-		if (KRequest::type() == 'HTTP') {
+  }     
+  
+  public function _actionForward(KCommandContext $context)
+  {
+    if (KRequest::type() == 'HTTP') {
       if($redirect = $this->getController()->getRedirect())
         KFactory::get('lib.joomla.application')->redirect($redirect['url'], $redirect['message'], $redirect['type']);  
-		}
+    }
 
-		if(KRequest::type() == 'AJAX') {
-			$view = KRequest::get('get.view', 'cmd');
-			$context->result = $this->getController()->execute('display', $context);
-			return $context->result;
-		}
-	} 
-	
-	protected function _actionRender(KCommandContext $context)
-	{
+    if(KRequest::type() == 'AJAX') {
+      $view = KRequest::get('get.view', 'cmd');
+      $context->result = $this->getController()->execute('display', $context);
+      return $context->result;
+    }
+  } 
+  
+  protected function _actionRender(KCommandContext $context)
+  {
     if($context->headers) 
     {
       foreach($context->headers as $name => $value) {
@@ -113,5 +113,5 @@ abstract class KDispatcherAbstract extends KControllerAbstract
 
     if(is_string($context->result))
       return $context->result;
-	}
+  }
 }
