f3af95655a27fe1c4cb42c99a6d4f1467e483072
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 11:57:12 -0700

    KFilterAlnum tabs to spaces

--- code/libraries/koowa/filter/alnum.php
+++ code/libraries/koowa/filter/alnum.php
@@ -3,15 +3,15 @@
 class KFilterAlnum extends KFilterAbstract
 {
   protected function _validate($value)
-	{
-		$value = trim($value);
-		
-		return ctype_alnum($value);
-	}                         
-	
-	protected function _sanitize($value)
-	{
-		$pattern 	= '/[^\w]*/';
-  	return preg_replace($pattern, '', $value);
-	}
+  {
+    $value = trim($value);
+    
+    return ctype_alnum($value);
+  }                         
+  
+  protected function _sanitize($value)
+  {
+    $pattern  = '/[^\w]*/';
+    return preg_replace($pattern, '', $value);
+  }
 }
