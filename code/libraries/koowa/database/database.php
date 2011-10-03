33c451404f6ab825e9af55b2d92fe220518a0a84
Ken Erickson <bookworm.productions@gmail.com>
2011-10-03 11:27:02 -0700

    KDatabase tabs to spaces

--- code/libraries/koowa/database/database.php
+++ code/libraries/koowa/database/database.php
@@ -2,24 +2,24 @@
 
 class KDatabase
 {
-	const OPERATION_SELECT = 1;
-	const OPERATION_INSERT = 2;
-	const OPERATION_UPDATE = 4;
-	const OPERATION_DELETE = 8;
-	const OPERATION_SHOW   = 16;
-	
-	const RESULT_STORE = 0;
-	const RESULT_USE   = 1;
-	
-	const FETCH_ROW         = 0;
-	const FETCH_ROWSET      = 1;
-	const FETCH_ARRAY       = 0;
-	const FETCH_ARRAY_LIST  = 1;
-	const FETCH_FIELD       = 2;
-	const FETCH_FIELD_LIST  = 3;
-	const FETCH_OBJECT      = 4;
-	const FETCH_OBJECT_LIST = 5;
-	
+  const OPERATION_SELECT = 1;
+  const OPERATION_INSERT = 2;
+  const OPERATION_UPDATE = 4;
+  const OPERATION_DELETE = 8;
+  const OPERATION_SHOW   = 16;
+  
+  const RESULT_STORE = 0;
+  const RESULT_USE   = 1;
+  
+  const FETCH_ROW         = 0;
+  const FETCH_ROWSET      = 1;
+  const FETCH_ARRAY       = 0;
+  const FETCH_ARRAY_LIST  = 1;
+  const FETCH_FIELD       = 2;
+  const FETCH_FIELD_LIST  = 3;
+  const FETCH_OBJECT      = 4;
+  const FETCH_OBJECT_LIST = 5;
+  
   const STATUS_LOADED   = 'loaded';
   const STATUS_DELETED  = 'deleted';
   const STATUS_CREATED  = 'created';
